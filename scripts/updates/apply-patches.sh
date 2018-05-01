d527495f4c24fb416ee5cbe79f532308b85291aa
Shea McKinney <sherakama@gmail.com>
2018-05-01 15:02:20 -0700

    fixup! wip.

--- scripts/updates/apply-patches.sh
+++ scripts/updates/apply-patches.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # patch --no-backup-if-mismatch -p1 < patches/enforce-paranoia.patch
 # patch --no-backup-if-mismatch -p1 < patches/core-profile-module-importance-max.patch
-patch --no-backup-if-mismatch -p1 < patches/saml.htaccess.patch
-patch --no-backup-if-mismatch -p1 < patches/combined.common.inc.patch
+patch --no-backup-if-mismatch -p1 < ../patches/saml.htaccess.patch
+patch --no-backup-if-mismatch -p1 < ../patches/combined.common.inc.patch
+exit 0;
