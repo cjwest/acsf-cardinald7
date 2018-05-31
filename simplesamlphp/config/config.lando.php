423b4910351bd57adaf5dc61009bf75f9049fc5b
Shea McKinney <sherakama@gmail.com>
2018-05-31 08:49:56 -0700

    SITES-000: Updates to lando saml config.

--- simplesamlphp/config/config.lando.php
+++ simplesamlphp/config/config.lando.php
@@ -6,6 +6,11 @@
 
 //$protocol = 'https://';
 //$config['baseurlpath'] = $protocol . $_SERVER['HTTP_HOST'] . '/simplesaml/';
+$database = !empty(getenv('DB_NAME')) ? getenv('DB_NAME') : 'drupal';
+$username = !empty(getenv('DB_USER')) ? getenv('DB_USER') : 'drupal';
+$password = !empty(getenv('DB_PASSWORD')) ? getenv('DB_PASSWORD') : 'drupal';
+$host = !empty(getenv('DB_HOST')) ? getenv('DB_HOST') : 'database';
+
 $config['debug'] = TRUE;
 $config['showerrors'] = TRUE;
 $config['errorreporting'] = TRUE;
@@ -17,15 +22,15 @@ $config['trusted.url.domains'] = array(
   'cardinald7.sws:80',
   'cardinald7.sws',
 );
-$config['database.dsn'] = 'mysql:host=database;dbname=drupal';
-$config['database.username'] = 'drupal';
-$config['database.password'] = 'drupal';
+$config['database.dsn'] = 'mysql:host=' . $host . ';dbname=' . $database;
+$config['database.username'] = $username;
+$config['database.password'] = $password;
 $config['enable.saml20-idp'] = TRUE;
 $config['session.duration'] = 8 * (60 * 60); // 8 hours.
 $config['session.datastore.timeout'] = (4 * 60 * 60); // 4 hours
 $config['session.state.timeout'] = (60 * 60); // 1 hour
 //$config['session.cookie.name'] = 'SimpleSAMLSessionID';
-//$config['session.cookie.lifetime'] = 1800;
+//$config['session.cookie.lifetime'] = 0;
 //$config['session.cookie.path'] = '/';
 //$config['session.cookie.secure'] = true;
 //$config['session.cookie_domain'] = 'cardinald7.sws';
@@ -37,7 +42,7 @@ $config['session.rememberme.enable'] = false;
 $config['session.rememberme.checked'] = false;
 $config['session.rememberme.lifetime'] = (14 * 86400);
 $config['store.type']          = 'sql';
-$config['store.sql.dsn']       = 'mysql:host=database;dbname=drupal';
+$config['store.sql.dsn']       = 'mysql:host=' . $host . ';dbname=' . $database;
 $config['store.sql.username']  = "drupal";
 $config['store.sql.password']  = "drupal";
 $config['store.sql.prefix']    = '';
