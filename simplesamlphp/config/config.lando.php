b977683ee943548c71081d179a6f5049bf3c0abf
Shea McKinney <sherakama@gmail.com>
2018-06-07 12:43:54 -0700

    Add new dependencies to profiles, disable diseval, disable saml debugging.

--- simplesamlphp/config/config.lando.php
+++ simplesamlphp/config/config.lando.php
@@ -11,12 +11,12 @@ $username = !empty(getenv('DB_USER')) ? getenv('DB_USER') : 'drupal';
 $password = !empty(getenv('DB_PASSWORD')) ? getenv('DB_PASSWORD') : 'drupal';
 $host = !empty(getenv('DB_HOST')) ? getenv('DB_HOST') : 'database';
 
-$config['debug'] = TRUE;
+// $config['debug'] = TRUE;
 $config['showerrors'] = TRUE;
 $config['errorreporting'] = TRUE;
-$config['logging.level'] = SimpleSAML\Logger::DEBUG;
+// $config['logging.level'] = SimpleSAML\Logger::DEBUG;
 $config['enable.http_post'] = TRUE;
-$config['debug.validatexml'] = TRUE;
+// $config['debug.validatexml'] = TRUE;
 $config['trusted.url.domains'] = array(
   'cardinald7.sws:443',
   'cardinald7.sws:80',
