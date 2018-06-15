35439e87406e20650b65d542c6423dd5aa4c01ef
Shea McKinney <sherakama@gmail.com>
2018-06-15 10:02:05 -0700

    fixup! update modules.

--- docroot/sites/all/libraries/stanford_install_tasks/Environment/ACSF/Install/SAMLSettings.php
+++ docroot/sites/all/libraries/stanford_install_tasks/Environment/ACSF/Install/SAMLSettings.php
@@ -24,6 +24,18 @@ class SAMLSettings extends AbstractInstallTask {
     $ah_env = getenv('AH_SITE_ENVIRONMENT');
     $pathtosimplesaml = "/var/www/html/" . $ah_stack . "." . $ah_env . "/simplesamlphp";
     variable_set('stanford_simplesamlphp_auth_installdir', $pathtosimplesaml);
+
+    // Enable workgroup api role mapping.
+    $cert_file = dirname(DRUPAL_ROOT) . "/simplesamlphp/cert/stanford_ssp.cert";
+    $key_file = dirname(DRUPAL_ROOT) . "/simplesamlphp/cert/stanford_ssp.key";
+    variable_set('stanford_ssp_workgroup_api_cert', $cert_file);
+    variable_set('stanford_ssp_workgroup_api_key', $key_file);
+    variable_set('stanford_ssp_role_map_source', 'workgroup');
+
+    // Add webservices role mapping.
+    $admin_role = user_role_load_by_name('administrator');
+    variable_set("stanford_simplesamlphp_auth_rolepopulation", $admin_role->rid . ":eduPersonEntitlement,=,itservices:webservices");
+
   }
 
   /**
