d4add32638065d7c6627b3ddbebd216452660f85
John Bickar <jbickar@stanford.edu>
2018-06-25 13:04:37 -0700

    SITES-336: Set paranoia_form_disabled message

---
+++ factory-hooks/post-settings-php/set-paranoia-message.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file
+ * This file sets the message users will see when they hit a form disabled by paranoia.
+ */
+$conf['paranoia_form_disabled_message'] = t('This form is disabled for security reasons. Please <a href="@snow_url">submit a help ticket</a> if you have questions.', array('@snow_url' => 'https://stanford.service-now.com/it_services?id=sc_cat_item&sys_id=abff87fa4f33f2009a9a97411310c723'));
+
