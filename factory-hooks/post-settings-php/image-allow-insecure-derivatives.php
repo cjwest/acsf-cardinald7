8792324eff00cfa71850d4d90c587cae990455f1
John Bickar <jbickar+github@stanford.edu>
2018-11-02 14:28:01 -0700

    Code comments for Future Us (#125)

--- factory-hooks/post-settings-php/image-allow-insecure-derivatives.php
+++ factory-hooks/post-settings-php/image-allow-insecure-derivatives.php
@@ -3,5 +3,23 @@
  * @file
  * This file sets the "image_allow_insecure_derivatives" variable to TRUE.
  */
+
+/** 
+ * NOTE: This option is contentious. See https://www.drupal.org/project/drupal/releases/7.20
+ * See also: https://github.com/SU-SWS/acsf-cardinald7/pull/124
+ *
+ * Pros:
+ * Eliminates confusing 403/500 errors when derivatives do not exist
+ * We had this set to TRUE on Sites 1.x, so it simplifies the migration
+ *
+ * Cons:
+ * Potential additional server overhead
+ * Potential DDOS vector
+ *
+ * Date of decision: 10/31/2018.
+ * Decided by: Shea McKinney and John Bickar.
+ *
+ */
+
 $conf['image_allow_insecure_derivatives'] = TRUE;
 
