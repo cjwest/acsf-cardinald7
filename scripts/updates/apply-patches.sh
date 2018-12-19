03796dbe3390a8c4401d11a351c6dbe958160add
Bill Minor <bill.minor@gmail.com>
2018-12-18 18:06:28 -0600

    SITES-1113: adding no_robots.txt to stop indexing of sites on dev and test (#155)

--- scripts/updates/apply-patches.sh
+++ scripts/updates/apply-patches.sh
@@ -3,4 +3,5 @@
 # patch --no-backup-if-mismatch -p1 < patches/core-profile-module-importance-max.patch
 patch -Ns -r "" --no-backup-if-mismatch -p1 < patches/saml.htaccess.patch
 patch -Ns -r "" --no-backup-if-mismatch -p1 < patches/combined.common.inc.patch
+patch -Ns -r "" --no-backup-if-mismatch -p1 < patches/no-robots.htaccess.patch
 exit 0;
